dd13588e985bf63be7fe5b5b9f1cfff1576e7e95
feraistanti <fistanti551@gmail.com>
2024-05-20 19:25:50 +0700

    anaqk

--- Agregasi/anak.h
+++ Agregasi/anak.h
@@ -1,4 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
 #ifndef ANAK_H
 #define ANAK_H
 
-using namespace std;
+class anak {
+public:
+	string nama;
+	anak(string pNama) :nama(pNama) {
+		cout << "Anak \"" << "\" ada\n";
+	}
+	~anak() {
+		cout << "Anak \"" << nama << "\" tidak ada\n";
+	}
+};
+#endif
